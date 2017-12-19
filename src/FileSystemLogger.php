c6a6ac8f2840e7b451a8b509cc12d3cd80ac96eb
v-dem <vitaly_demyanenko@yahoo.com>
2017-12-19 22:16:44 +0200

    #1 done, #2 #7 almost done

--- src/FileSystemLogger.php
+++ src/FileSystemLogger.php
@@ -7,152 +7,26 @@ use Psr\Log\LogLevel;
 
 use queasy\config\ConfigInterface;
 
-class Logger implements AbstractLogger
+class FileSystemLogger extends LoggerAggregate
 {
-
-    const DEFAULT_TIME_FORMAT = 'Y-m-d H:i:s.u T';
-    const DEFAULT_HISTORY_LENGTH = 10;
-
-    public static function level2int($level)
-    {
-        switch ($level) {
-            case LogLevel::DEBUG:
-                return 0;
-            case LogLevel::INFO:
-                return 1;
-            case LogLevel::NOTICE:
-                return 2;
-            case LogLevel::WARNING:
-                return 3;
-            case LogLevel::ERROR:
-                return 4;
-            case LogLevel::CRITICAL:
-                return 5;
-            case LogLevel::ALERT:
-                return 6;
-            case LogLevel::EMERGENCY:
-                return 7;
-            default:
-                return 0;
-        }
-    }
+    const DEFAULT_PATH = 'logs/debug.log';
 
     private $path;
-    private $debugMode;
-    private $processName;
-    private $sendEmail;
-    private $mailTo;
-    private $timeFormat;
-    private $historyLength;
-
-    private $history = array();
 
     public function __construct(ConfigInterface $config)
     {
+        parent::__construct($config);
+
         $this->path = $config->get('path', self::DEFAULT_PATH);
-        $this->debugMode = $config->get('debugMode', false);
-        $this->processName = $config->get('processName', '');
-        $this->sendEmail = $config->get('sendEmail', false);
-        $this->mailTo = $config->get('mailTo', null);
-        $this->timeFormat = $config->get('timeFormat', self::DEFAULT_TIME_FORMAT);
-        $this->historyLength = $config->get('historyLength', self::DEFAULT_HISTORY_LENGTH);
     }
 
     public function log($level, $message, array $context = array())
     {
-        
+        parent::log($level, $message, $context);
+
+        $preparedMessage = $this->prepareMessage($level, $message, $context);
+
+        file_put_contents($this->path, $preparedMessage, FILE_APPEND | LOCK_EX);
     }
-
-    private static function message($level, $message, $sendEmail = true, $prefix = '')
-    {
-        switch ($level) {
-            case self::LEVEL_INFO:
-                $messageLevel = 'INFO';
-                break;
-            case self::LEVEL_ERROR:
-                $messageLevel = 'ERROR';
-                break;
-            case self::LEVEL_WARNING:
-                $messageLevel = 'WARNING';
-                break;
-            case self::LEVEL_DEBUG:
-            default:
-                $messageLevel = 'DEBUG';
-        }
-
-        $ipAddress = '';
-        if (isset($_SERVER) && isset($_SERVER['REMOTE_ADDR'])) {
-            $ipAddress = sprintf(' [%s]', $_SERVER['REMOTE_ADDR']);
-        }
-
-        $message = sprintf(
-            '%s%s [%s]%s [%s] %s%s',
-            self::getTimeString(),
-            (empty(self::$processName)? '': ' ' . self::$processName),
-            session_id(),
-            $ipAddress,
-            $messageLevel,
-            $prefix,
-            ((is_object($message) || is_array($message))? print_r($message, true): $message)
-        );
-
-        self::appendLogMessage($message);
-
-        if ((self::LEVEL_ERROR == $level)
-                && !empty(self::$sendEmail)
-                && self::$sendEmail
-                && $sendEmail) {
-            if (!empty(self::$mailTo)) {
-                $historyMessage = '';
-                foreach(self::$history as $message) {
-                    $historyMessage .= $message . "\n";
-                }
-
-                if (!mail(self::$mailTo, (empty(self::$processName)? '': self::$processName . ': ') . 'Error Report', $historyMessage)) {
-                    self::message(self::LEVEL_ERROR, 'Failed sending email report to admin.', false);
-                }
-            } else {
-                self::message(self::LEVEL_WARNING, 'Cannot send email report to admin. No recipients configured.', false);
-            }
-        }
-    }
-
-    private static function appendLogMessage($message)
-    {
-        if (self::$historyLength <= count(self::$history)) {
-            array_shift(self::$history);
-        }
-
-        self::$history[] = $message;
-
-        file_put_contents(self::$path, $message . "\n", FILE_APPEND | LOCK_EX);
-    }
-
-    private static function getTimeString()
-    {
-        $uTimestamp = microtime(true);
-        $timestamp = floor($uTimestamp);
-        $milliseconds = '' . round(($uTimestamp - $timestamp) * 1000000);
-        $milliseconds = str_pad($milliseconds, 6, '0');
-        return date(preg_replace('/(?<!\\\\)u/', $milliseconds, self::$timeFormat), $timestamp);
-    }
-
 }
-
-$loggerOldErrorHandler = set_error_handler(function($errLevel, $errMessage, $errFile, $errLine, $errContext) {
-    global $loggerOldErrorHandler;
-
-    Logger::error("[$errFile:$errLine] ($errLevel) $errMessage");
-
-    if (!is_null($loggerOldErrorHandler)) {
-        return $loggerOldErrorHandler($errLevel, $errMessage, $errFile, $errLine, $errContext);
-    }
-}, E_ALL | E_STRICT);
-
-set_exception_handler(function($ex) {
-    Logger::error('Uncaught exception: ' . $ex->getMessage());
-    Logger::info('Exception details: ' . print_r($ex, true));
-});
-
-Logger::init();
 
--- src/LoggerAggregate.php
+++ src/LoggerAggregate.php
@@ -2,19 +2,21 @@
 
 namespace queasy\log;
 
+use Exception;
+use InvalidArgumentException as StandardInvalidArgumentException;
+
 use Psr\Log\InvalidArgumentException;
 use Psr\Log\AbstractLogger;
 use Psr\Log\LogLevel;
 
 use queasy\config\ConfigInterface;
 
-class LoggerAggregate implements AbstractLogger
+class LoggerAggregate extends AbstractLogger
 {
-
-    const DEFAULT_MIN_LEVEL = 'debug';
-    const DEFAULT_MAX_LEVEL = 'emergency';
+    const DEFAULT_MIN_LEVEL = LogLevel::DEBUG;
+    const DEFAULT_MAX_LEVEL = LogLevel::EMERGENCY;
     const DEFAULT_TIME_FORMAT = 'Y-m-d H:i:s.u T';
-    const DEFAULT_MESSAGE_FORMAT = '%1$s %2$s [%3$s] %4$s [%5$s] %6$s';
+    const DEFAULT_MESSAGE_FORMAT = '%1$s %2$s [%3$s] [IP %4$s] [%5$s] %6$s %7$s'; // 1) time, 2) process name, 3) session id, 4) ip address, 5) log level, 6) message, 7) context
 
     public static function level2int($level)
     {
@@ -62,57 +64,56 @@ class LoggerAggregate implements AbstractLogger
             if (($section instanceof ConfigInterface)
                     && isset($section['logger'])) {
                 $className = $section['logger'];
+                if (!class_exists($className)) {
+                    throw new InvalidArgumentException(sprintf('Logger class "%s" does not exist.', $className));
+                }
+
                 $interfaces = class_implements($className);
                 if (!$interfaces || !isset($interfaces['Psr\Log\LoggerInterface'])) {
                     throw new InvalidArgumentException(sprintf('Logger class "%s" does not implement Psr\Log\LoggerInterface.', $className));
                 }
 
-                $logger = new $className($section);
-
-                $this->loggers[] = new LoggerRecord($logger, $section);
+                $this->loggers[] = new $className($section);
             }
         }
     }
 
-    public function minLevel()
-    {
-        return $this->minLevel;
-    }
-
-    public function maxLevel()
-    {
-        return $this->maxLevel;
-    }
-
-    public function processName()
-    {
-        return $this->processName;
-    }
-
-    public function timeFormat()
-    {
-        return $this->timeFormat;
-    }
-
-    public function messageFormat()
-    {
-        return $this->messageFormat;
-    }
-
     public function log($level, $message, array $context = array())
     {
-        foreach ($this->loggers as $loggerRecord) {
-            $config = $loggerRecord->config();
-            if ((self::level2int($level) >= self::level2int($config->minLevel))
-                    && (self::level2int($level) <= self::level2int($config->maxLevel))) {
-                $logger = $loggerRecord->logger();
-
+        foreach ($this->loggers as $logger) {
+            if ((self::level2int($level) >= self::level2int($logger->minLevel()))
+                    && (self::level2int($level) <= self::level2int($logger->maxLevel()))) {
                 $logger->log($level, $message, $context);
             }
         }
     }
 
-    protected function getTimeString()
+    protected function minLevel()
+    {
+        return $this->minLevel;
+    }
+
+    protected function maxLevel()
+    {
+        return $this->maxLevel;
+    }
+
+    protected function processName()
+    {
+        return $this->processName;
+    }
+
+    protected function timeFormat()
+    {
+        return $this->timeFormat;
+    }
+
+    protected function messageFormat()
+    {
+        return $this->messageFormat;
+    }
+
+    protected function logTime()
     {
         $uTimestamp = microtime(true);
         $timestamp = floor($uTimestamp);
@@ -122,5 +123,66 @@ class LoggerAggregate implements AbstractLogger
         return date(preg_replace('/(?<!\\\\)u/', $milliseconds, $this->timeFormat()), $timestamp);
     }
 
+    protected function sessionId()
+    {
+        return session_id();
+    }
+
+    protected function ipAddress()
+    {
+        if (isset($_SERVER) && isset($_SERVER['REMOTE_ADDR'])) {
+            return $_SERVER['REMOTE_ADDR'];
+        }
+    }
+
+    protected function logLevel($level)
+    {
+        return strtoupper($level);
+    }
+
+    protected function message($message)
+    {
+        return $message;
+    }
+
+    protected function context(array $context = null)
+    {
+        if (is_null($context)) {
+            return '';
+        }
+
+        $result = '';
+        $ex = null;
+        if (isset($context['exception'])) {
+            $ex = $context['exception'];
+
+            if (interface_exists('\Throwable') && is_subclass_of($ex, '\Throwable')
+                    || ($ex instanceof Exception)) {
+                $result .= get_class($ex) . ': ' . $ex->getMessage() . ' in ' . $ex->getFile() . '(' . $ex->getLine() . ')' . PHP_EOL;
+                $result .= 'Stack trace:' . PHP_EOL;
+                $result .= $ex->getTraceAsString() . PHP_EOL;
+
+                unset($context['exception']);
+            } else {
+                throw new StandardInvalidArgumentException('Value of \'exception\' key in $context does not contain valid Throwable or Exception instance.');
+            }
+        }
+
+        return (count($context)? PHP_EOL . print_r($context, true): '') . $result;
+    }
+
+    protected function prepareMessage($level, $message, array $context = array())
+    {
+        return sprintf(
+            $this->messageFormat(),
+            $this->logTime(),
+            $this->processName(),
+            $this->sessionId(),
+            $this->ipAddress(),
+            $this->logLevel($level),
+            $this->message($message),
+            $this->context($context)
+        );
+    }
 }
 
