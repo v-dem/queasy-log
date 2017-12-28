27a4b746e257d330d9d94b0fd84ca2364db2583a
v-dem <vitaly_demyanenko@yahoo.com>
2017-12-28 11:32:16 +0200

    chain calls are possible now, not supported by Psr\Log

--- src/Logger.php
+++ src/Logger.php
@@ -191,6 +191,63 @@ class Logger extends AbstractLogger
         return false;
     }
 
+    public function emergency($message, array $context = array())
+    {
+        parent::emergency($message, $context);
+
+        return $this;
+    }
+
+    public function alert($message, array $context = array())
+    {
+        parent::alert($message, $context);
+
+        return $this;
+    }
+
+    public function critical($message, array $context = array())
+    {
+        parent::critical($message, $context);
+
+        return $this;
+    }
+
+
+    public function error($message, array $context = array())
+    {
+        parent::error($message, $context);
+
+        return $this;
+    }
+
+    public function warning($message, array $context = array())
+    {
+        parent::warning($message, $context);
+
+        return $this;
+    }
+
+    public function notice($message, array $context = array())
+    {
+        parent::notice($message, $context);
+
+        return $this;
+    }
+
+    public function info($message, array $context = array())
+    {
+        parent::info($message, $context);
+
+        return $this;
+    }
+
+    public function debug($message, array $context = array())
+    {
+        parent::debug($message, $context);
+
+        return $this;
+    }
+
     /**
      * Exceptions handler.
      *
