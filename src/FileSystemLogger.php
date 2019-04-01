a20dd396d0a8bf98755ca2ff4c677b3dfe1377ee
v-dem <vitaly_demyanenko@yahoo.com>
2019-04-01 20:10:29 +0300

    fixed php5 incompatibility issue 2

--- src/FileSystemLogger.php
+++ src/FileSystemLogger.php
@@ -42,7 +42,7 @@ class FileSystemLogger extends Logger
      */
     protected function path()
     {
-        return $this->config()('path', static::DEFAULT_PATH);
+        return $this->config()->get('path', static::DEFAULT_PATH);
     }
 }
 
